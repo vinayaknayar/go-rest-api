d400ead84577de2e9a40e26d279dfb02f5f4fa08
Vinayak Nayar <nayarvinayak03@gmail.com>
2023-05-07 04:08:29 +0530

    chore: swagger server setup, server initiated

--- go.mod
+++ go.mod
@@ -1,3 +1,33 @@
-module github.com/vinayaknayar/go-http
+module github.com/vinayaknayar/go-rest-api
 
 go 1.20
+
+require (
+	github.com/go-openapi/errors v0.20.3
+	github.com/go-openapi/loads v0.21.2
+	github.com/go-openapi/runtime v0.26.0
+	github.com/go-openapi/spec v0.20.9
+	github.com/go-openapi/strfmt v0.21.7
+	github.com/go-openapi/swag v0.22.3
+	github.com/jessevdk/go-flags v1.5.0
+	golang.org/x/net v0.9.0
+)
+
+require (
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/go-openapi/analysis v0.21.4 // indirect
+	github.com/go-openapi/jsonpointer v0.19.6 // indirect
+	github.com/go-openapi/jsonreference v0.20.2 // indirect
+	github.com/go-openapi/validate v0.22.1 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
+	github.com/rogpeppe/go-internal v1.10.0 // indirect
+	go.mongodb.org/mongo-driver v1.11.4 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- internal/main.go
+++ internal/main.go
@@ -5,15 +5,49 @@ import (
 	"html"
 	"log"
 	"net/http"
+
+
+    "github.com/go-openapi/loads"
+    "github.com/go-openapi/runtime/middleware"
+    "github.com/vinayaknayar/go-rest-api/pkg/swagger/server/restapi"
+    "github.com/vinayaknayar/go-rest-api/pkg/swagger/server/restapi/operations"
 )
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+	// 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	// })
 
-	log.Println("Listening on localhost:8080")
+	// log.Println("Listening on localhost:8080")
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	// log.Fatal(http.ListenAndServe(":8080", nil))
+
+	//intialize swagger
+	swaggerSpec, err := loads.Analyzed(restapi.SwaggerJSON,"")
+	if err != nil{
+		log.Fatalln(err)
+	}
+
+	api := operations.NewHelloAPIAPI(swaggerSpec)
+	server := restapi.NewServer(api)
+
+	defer func ()  {
+		if err := server.Shutdown(); err != nil{
+			//error handle
+			log.Fatalln(err)
+		}	
+	}()
+	
+	server.Port = 8080
+
+	api.CheckHealthHandler = operations.CheckHealthHandlerFunc(Health)
+	api.GetHelloUserHandler = operations.GetHelloUserHandlerFunc(GetHelloUser)
+	api.GetGopherNameHandler = operations.GetGopherNameHandlerFunc(GetGopherByName)
+
+	//Start server which listening
+	if err := server.Serve(); err != nil{
+		log.Fatalln(err)
+	}
+	
 }
