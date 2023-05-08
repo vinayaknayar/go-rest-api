c4f76ab73eb791d84e1b084463ae0beb0e5c348e
Vinayak Nayar <nayarvinayak03@gmail.com>
2023-05-08 16:20:59 +0530

    chore: commented code removed

--- internal/main.go
+++ internal/main.go
@@ -12,15 +12,7 @@ import (
 )
 
 func main() {
-
-	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-	// 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	// })
-
-	// log.Println("Listening on localhost:8080")
-
-	// log.Fatal(http.ListenAndServe(":8080", nil))
-
+	
 	//intialize swagger
 	swaggerSpec, err := loads.Analyzed(restapi.SwaggerJSON, "")
 	if err != nil {
